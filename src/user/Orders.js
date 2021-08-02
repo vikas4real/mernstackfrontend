56dcf43582f60e00e35fde565d8f73a9e5dcd7a0
Vikas Pandey <vikas000pandey@gmail.com>
2021-08-02 13:09:52 +0530

    order style change

--- src/user/Orders.js
+++ src/user/Orders.js
@@ -3,6 +3,7 @@ import { isAuthenticated } from "../auth/helper";
 import Base from "../core/Base";
 import { getAllUserOrders } from "./helper/userapicalls";
 import moment from "moment";
+import "../core/css/order-style.css";
 const Orders = () => {
    const [orders, setOrders] = useState([]);
    const userId = isAuthenticated() && isAuthenticated().user._id;
@@ -23,50 +24,66 @@ const Orders = () => {
    }, []);
 
    return (
-      <div className="display-flex">
+      <div>
          <Base />
          <div className="row">
             <div className="col-12">
                <h2 className="text-center  my-3">All Orders</h2>
-               <table className="table text-center">
-                  <thead>
-                     <tr>
-                        <th scope="col">Order ID</th>
-                        <th scope="col">Order Status</th>
-                        <th scope="col">Product Name</th>
-                        <th scope="col">Transaction ID</th>
-                        <th scope="col">Amount</th>
-                        <th scope="col">Date & Time</th>
-                     </tr>
-                  </thead>
 
-                  {orders.map((order, index) => {
-                     return (
-                        <tbody>
-                           <tr>
-                              <th key={index} scope="row">
-                                 {order._id}
-                              </th>
-                              <td>{order.status}</td>
-                              <td>
-                                 {order.products.map((product, index) => {
-                                    return (
-                                       <span key={index}>
+               {orders.map((order, index) => {
+                  return (
+                     <div key={index} class="order-card">
+                        <div class="title">Order Details</div>
+                        <div class="info">
+                           <div class="row">
+                              <div class="col-7">
+                                 <span id="heading">Date</span>
+                                 <br />
+                                 <span id="details">
+                                    {moment(order.updatedAt).format("L")}
+                                 </span>
+                              </div>
+                              <div class="col-5 pull-right">
+                                 <span id="heading">Order No.</span>
+                                 <br />
+                                 <span id="details">{order._id}</span>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="pricing">
+                           {order.products.map((product, index) => {
+                              return (
+                                 <div class="row">
+                                    <div class="col-9">
+                                       <span id="name" key={index}>
                                           <span>{index + 1}. </span>
                                           {product.name}
                                           <p></p>
                                        </span>
-                                    );
-                                 })}
-                              </td>
-                              <td>{order.txn_id}</td>
-                              <td>₹ {order.total_amount}</td>
-                              <td>{moment(order.updatedAt).format("L")}</td>
-                           </tr>
-                        </tbody>
-                     );
-                  })}
-               </table>
+                                    </div>
+                                    <div class="col-3">
+                                       <span id="price">₹ {product.price}</span>
+                                    </div>
+                                 </div>
+                              );
+                           })}
+                        </div>
+                        <div class="total">
+                           <div class="row">
+                              <div class="col-9">
+                                 <span>Txn ID: </span>
+                                 <span style={{ fontWeight: "normal" }}>
+                                    {order.txn_id}
+                                 </span>
+                              </div>
+                              <div class="col-3">
+                                 <big>₹ {order.total_amount}</big>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  );
+               })}
             </div>
          </div>
       </div>
